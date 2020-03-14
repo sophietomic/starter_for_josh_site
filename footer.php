34fd4ec79d87110edb9fb554d78621b0a7e73ecd
sophietomic <sophietomic@gmail.com>
2020-03-14 21:55:19 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="contsiner-fluid"><!--div for   the main image to stretch to the edge of page-->
+      <div class="container"><!-- to hold content in the middle of the page-->
+          <div class="row"><!-- a row that gives us access to the BS columns-->
+          <div class="col-md-6 text-center footer-content">
+             <p> <?php bloginfo('name'); ?></p>
+                             <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss” >
+              </div>
+              <div class="col-md-6 text-center footer-content">
+             <p> Terms and Conditions</p>
+                  <img src="images/linkedin.jpg" alt="linkedin">
+                  <img src="images/facebook.jpg" alt="facebook">
+              </div>
+              
+          </div>      
+          </div>
+      </footer>
+
+ 
+  </body>
+</html>
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
