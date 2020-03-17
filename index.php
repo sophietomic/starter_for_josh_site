1e7bc316fc90da0ad4edbf1c992f5874aeeca2d1
sophietomic <sophietomic@gmail.com>
2020-03-17 11:56:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
-  
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-      <link href="https://fonts.googleapis.com/css?family=Raleway:400,800&display=swap" rel="stylesheet">
-      <link rel="stylesheet" href="style.css" /> 
-      <link rel="stylesheet" href="responsive.css" /> 
-  </head>
-  <body>
-       <header class="container-fluid herobg">
-      <nav class="navbar">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-        <a class="navbar-brand" href="#"><span class="josh">JOSH  </span>
-            
-            <span class="orange">WHITKIN</span>
-        </a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">HOME</a></li>
-        <li><a href="#">PROJECTS</a></li>
-        <li><a href="#">PUBLICATIONS</a></li>
-        <li><a href="#">CV</a></li>
-          <li><a href="#">CONTACT</a></li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-  
-      <div class="container">
-       <h1>Game Designs For the future</h1>
-       </div>
-      </header>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
       
       <section class="container-fluid aboutbg text-center">
          
@@ -86,7 +33,7 @@
                  </div>
                 
                  <div class="col-md-4 article-cent">
-                     <img class="img-responsive" alt=“Portrait” src="Images/Portrait.png">
+                     <img class="img-responsive" alt=“Portrait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/portrait.jpg">
                  <h4>A PORTRAIT</h4>
                  <p class="date "> 12 December 2020</p>
                      <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
@@ -95,7 +42,8 @@
                    
                  
                 <div class="col-md-4 article-cent">
-                     <img class="img-responsive" alt=“Model” src="Images/Model.png">
+                     <img class="img-responsive" alt=“Model”
+                    src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
                  <h4>A MODEL</h4>
                  <p class="date"> 12 December 2020</p>
                      <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
@@ -105,7 +53,7 @@
                      
                  
                   <div class="col-md-4 article-cent">
-                     <img class="img-responsive" alt=“Games-Design” src="Images/Games-Design.png">
+                     <img class="img-responsive" alt=“Games-Design” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/game.jpg">
                  <h4>GAMES ART</h4>
                   <p class="date"> 12 December 2020</p>
                      <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
@@ -115,30 +63,5 @@
                      
                </div><!--row-->
            </div><!--container-->
-      </section>
-      
-      
-      <footer class="contsiner-fluid"><!--div for   the main image to stretch to the edge of page-->
-      <div class="container"><!-- to hold content in the middle of the page-->
-          <div class="row"><!-- a row that gives us access to the BS columns-->
-          <div class="col-md-6 text-center footer-content">
-             <p> JOSH WHITKIN</p>
-              <img src="Images/tumblr.jpg" alt="tumblr">
-              <img src="images/connect.jpg" alt="connect">
-              </div>
-              <div class="col-md-6 text-center footer-content">
-             <p> Terms and Conditions</p>
-                  <img src="images/linkedin.jpg" alt="linkedin">
-                  <img src="images/facebook.jpg" alt="facebook">
-              </div>
-              
-          </div>      
-          </div>
-      </footer>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+      </section>   
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
