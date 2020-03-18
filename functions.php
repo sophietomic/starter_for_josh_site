92146ec0856b0bc2a9b4a7db52435007eab75923
sophietomic <sophietomic@gmail.com>
2020-03-18 22:44:26 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,7 +6,5 @@ wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
 wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
 wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
 }
- 
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
-
 ?>
