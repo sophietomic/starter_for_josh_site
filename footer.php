61fa793139130cfa649df37a354d4ab664514441
sophietomic <sophietomic@gmail.com>
2020-03-18 22:44:24 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,24 @@
-<footer class="contsiner-fluid"><!--div for   the main image to stretch to the edge of page-->
-      <div class="container"><!-- to hold content in the middle of the page-->
-          <div class="row"><!-- a row that gives us access to the BS columns-->
-          <div class="col-md-6 text-center footer-content">
-             <p> <?php bloginfo('name'); ?></p>
-                             <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+<p> <?php bloginfo('name'); ?></p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
                 <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss” >
-              </div>
-              <div class="col-md-6 text-center footer-content">
-             <p> Terms and Conditions</p>
-                  <img src="images/linkedin.jpg" alt="linkedin">
-                  <img src="images/facebook.jpg" alt="facebook">
-              </div>
-              
-          </div>      
-          </div>
-      </footer>
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished//images/linkedin.jpg" alt=“linkedin” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg" alt=“facebook” >
+            </div>
 
- 
-  </body>
-</html>
+        </div>
+    </div>
+</footer>
 
-<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
+<!—DELETE ALL JAVASCRIPT -->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
