23fbe1ffa585f16ef9543d8def08d1e62f8d816c
sophietomic <sophietomic@gmail.com>
2020-04-23 17:36:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,86 +1,49 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
-      
-      <section class="container-fluid aboutbg text-center">
-         
-    <div class="container">
-          
-         <?php get_header(); /* Tells WordPress to include header.php */ ?>
      <section class="container-fluid aboutbg text-center">
         <div class="container">
-            
-        
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=2');//look for posts that have the category of 2
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
-            <div><p class="about-text"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>    
-    	
- <div class="blocker"></div>
-            
+            <h2> GAMES &amp; LEVEL DESIGNER </h2>
+            <p class="particle">...and particle wizard</p>
+            <p class="about-text">My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
+            <div class="blocker"></div>
+
       </div><!--  container-->
     </section>
-
-        
-        <div class="blocker"></div>
-             
-          </div><!--          container-->
-      </section>
-      
-      <section class="container-fluid">
-      
-      <div class="container">
-         <div class="row">      
-          </div>
-          </div>
-      
-      </section>
-      
-      
-       <section class="container-fluid articlesbg">
-      <div class="container">
-             <div class="row"><!-- a row that gives us access to the BS columns-->
-         <div>       
-             <h3 class="text-center article-title">LATEST ARTICLES</h3>        
-                 </div>
-                
-                 <div class="col-md-4 article-cent">
-                     <img class="img-responsive" alt=“Portrait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/portrait.jpg">
-                 <h4>A PORTRAIT</h4>
-                 <p class="date "> 12 December 2020</p>
-                     <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
-                     <p class="read-more"> CONTINUE READING</p>
-                 </div>
-                   
-                 
+<section class="container-fluid ">
+        <div class="container">
+            <div class="row"><!--  a row that gives us access to the BS columns-->
+                       <div class="article-title text-center">
+                    <h3>LATEST ARTICLES</h3>
+                </div>
                 <div class="col-md-4 article-cent">
-                     <img class="img-responsive" alt=“Model”
-                    src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
-                 <h4>A MODEL</h4>
-                 <p class="date"> 12 December 2020</p>
-                     <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
-                     
-                     <p class="read-more"> CONTINUE READING</p>
-                 </div>
-                     
-                 
-                  <div class="col-md-4 article-cent">
-                     <img class="img-responsive" alt=“Games-Design” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/game.jpg">
-                 <h4>GAMES ART</h4>
-                  <p class="date"> 12 December 2020</p>
-                     <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
-                  
-                 <p class="read-more"> CONTINUE READING</p>
-                 </div>
-                     
-               </div><!--row-->
-           </div><!--container-->
-      </section>   
+                                        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ portrait.jpg">
+                    <h4>A PORTRAIT</h4>
+                                       <p class="date">12 December 2020</p>
+                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                               <p class="readmore">CONTINUE READING</p>
+                </div>
+                <div class="col-md-4 article-cent">
+                                        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
+                    <h4>A MODEL</h4>
+                                        <p class="date">12 December 2020</p>
+                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                                   <p class="readmore">CONTINUE READING</p>
+                </div>
+                <div class="col-md-4 article-cent">
+                                        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg">
+
+                    <h4>A GAMES DESIGN</h4>
+                                        <p class="date">12 December 2020</p>
+                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                                      <p class="readmore">CONTINUE READING</p>
+                </div>
+                
+                    </div><!-- row-->
+            </div><!-- container-->
+        
+    </section><!-- container-fluid-->
+
+
 <?php get_footer(); /* Tells WordPress to include footer.php */ ?>
