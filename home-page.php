d5033dbad6e25381f8c635ab535062ecb31c63fe
sophietomic <sophietomic@gmail.com>
2020-04-24 14:18:48 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -6,7 +6,9 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
      <section class="container-fluid aboutbg text-center">
         <div class="container">
-              <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+            <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+<p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
             <div class="blocker"></div>
 
       </div><!--  container-->
